cc956bca589f4ab2e24161ec58bb1cfa71f7ef82
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-08-19 12:09:27 -0400

    build matrix from either size or input grid

--- code-challenge/src/classes.js
+++ code-challenge/src/classes.js
@@ -1,16 +1,21 @@
+import {
+  alive,
+  dead
+} from "./theme.js";
+
 export class Cell {
   constructor(value, row, column, up, down, left, right) {
     this.value = value || false,
-    this.location = {
-      row: row,
-      column: column
-    }, 
-    this.neighbors = {
-      up: up || null,
-      down: down || null,
-      left: left || null,
-      right: right || null,
-    }
+      this.location = {
+        row: row,
+        column: column,
+      },
+      this.neighbors = {
+        up: up || null,
+        down: down || null,
+        left: left || null,
+        right: right || null,
+      }
   }
 
   aliveNeighbors() {
@@ -21,15 +26,37 @@ export class Cell {
   }
 }
 
+
 export class Grid {
-  constructor(size, cells) {
-    this.size = size || null,
-    this.cells = cells || null
+  constructor(input = 2) {
+    this.size = (typeof (input) === "number") ? input : input.length
+    // this.size = size || (inputGrid ? inputGrid.length: 2),
+    this.cells = this.generateCells(input)
+  }
+  buildFromSize(size) {
+    const board = []
+    for (let i = 0; i < size; i++) {
+      board.push([])
+      for (let j = 0; j < size; j++) {
+        const life = (Math.random() > 0.5) ? alive : dead
+        board[i].push(new Cell(life, i, j))
+      }
+    }
+    return board
+  }
+  buildFromGrid(input) {
+    return input.map((row) => {
+      return row.map(element => new Cell(element))
+    })
   }
 
-  generateCells() {
-    const grid = {}
-    
-    return grid;
+
+
+  generateCells(input) {
+    return (typeof (input) === "number") ?
+      // create cells from size parameters
+      this.buildFromSize(input) :
+      // create cells from inputGrid
+      this.buildFromGrid(input)
   }
 }
